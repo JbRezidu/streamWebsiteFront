4cea59693f358a8c486e48d35719868e9b8ca81e
frokan <frokangaming@gmail.com>
2018-02-08 21:44:24 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php 
+/* partie du code php ci-dessous. */
+
+/* Je creer un texte :*/ // ou je peux le faire en commentaire monoligne !
+echo "Ceci est un test !";
+/* marche avec des balises ! ainsi qu'un guillemet !*/
+echo "Ceci est un texte <strong>\"en gras !\"</strong>";
+/* afficher la date facilement !*/
+echo date('d/m/Y h:i:s')
+
+
+
+
+
+
+
+
+
+
+
+
+?>
